f6c4a76e437f1757d6c7250c26b6a835099224a6
Zoe F <fortin.zoe@student.greenriver.edu>
2023-01-06 14:50:40 -0500

    Create home page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pick A Color</title>
+</head>
+<body>
+    <h1>Pick a Color</h1>
+</body>
+</html>
