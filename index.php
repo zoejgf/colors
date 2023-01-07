9c483f7b9f484213c7fae1d10283eae652cd95dc
Zoe F <fortin.zoe@student.greenriver.edu>
2023-01-07 01:51:47 -0500

    Create colors form page

--- index.php
+++ index.php
@@ -9,5 +9,12 @@
 </head>
 <body>
     <h1>Pick a Color</h1>
+    <form method="post" action="confirm.php">
+        <label><input type="radio" value="red" name="color">Red</label><br>
+        <label><input type="radio" value="blue" name="color">Blue</label><br>
+        <label><input type="radio" value="yellow" name="color">Yellow</label><br>
+        <input type="submit" value="Submit">
+
+    </form>
 </body>
 </html>
